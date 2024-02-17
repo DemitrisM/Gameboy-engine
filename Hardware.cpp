cedbf0909bca9caffc5c3974f24dd48afd086866
DemitrisM <mavridis.d1@live.unic.ac.cy>
2024-02-17 11:07:23 +0200

    Defined the 10 main registers

--- Hardware.cpp
+++ Hardware.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
+#include <cstdint>
 using namespace std;
 
+class Registers{
+    public : 
+
+    private : 
+        uint8_t A;
+        uint8_t B;
+        uint8_t C;
+        uint8_t D;
+        uint8_t E;
+        uint8_t H; 
+        uint8_t L;
+        uint8_t F;
+        uint16_t SP;
+        uint16_t PC;
+};
 int main(){
     cout<<"hello ";
 }
-
