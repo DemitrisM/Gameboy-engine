12fe27899c9607a0342ba20965fb1c02940d346f
DemitrisM <mavridis.d1@live.unic.ac.cy>
2024-02-17 13:03:19 +0200

    Implemented the four flags for the F register

--- Hardware.cpp
+++ Hardware.cpp
@@ -4,19 +4,67 @@ using namespace std;
 
 class Registers{
     public : 
-
+        void setZeroFlag(bool flag){
+            if(flag){
+                F |= 0x80;
+            }
+            else{
+                F &= ~0x80;
+            }
+        }
+        void setSubtractFlag(bool flag){
+            if(flag){
+                F |= 0x40;
+            }
+            else{
+                F &= ~0x40;
+            }
+        }
+        void setHalfCarryFlag(bool flag){
+            if(flag){
+                F |= 0x20;
+            }
+            else{
+                F &= ~0x20;
+            }
+        }
+        void setCarryFlag(bool flag){
+            if(flag){
+                F |= 0x10;
+            }
+            else{
+                F &= ~0x10;
+            }
+        }
+        bool getZeroFlag() const{
+            return F & 0x80;
+        }
+        bool getSubtractFlag() const{
+            return F & 0x40;
+        }
+        bool getHalfCarryFlag() const{
+            return F & 0x20;
+        }
+        bool getCarryFlag() const{
+            return F & 0x10;
+        }
     private : 
-        uint8_t A;
-        uint8_t B;
-        uint8_t C;
-        uint8_t D;
-        uint8_t E;
-        uint8_t H; 
-        uint8_t L;
-        uint8_t F;
-        uint16_t SP;
-        uint16_t PC;
+        uint8_t A, B, C, D, E, H, L, F;
+        // Stack Pointer and Program Counter
+        uint16_t SP, PC;
+        // Register Pair below
+        uint16_t BC, DE, HL;
+    
 };
+union RegisterPair{
+    //set low and high 8 bit registers or 16 bit register
+    uint16_t reg16;
+    struct{
+        uint8_t low;
+        uint8_t high;
+    };
+};
+
 int main(){
     cout<<"hello ";
 }
