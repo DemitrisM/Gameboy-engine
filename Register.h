01d6286aaa6ab5beca4dd9f56f367954768b9e9c
DemitrisM <mavridis.d1@live.unic.ac.cy>
2024-02-18 21:49:33 +0200

    Reconstructed Registers.cpp

--- Register.h
+++ Register.h
@@ -4,6 +4,21 @@
 
 class Registers{
     public : 
+        uint8_t A, B, C, D, E, H, L, F;
+        // Stack Pointer and Program Counter
+        uint16_t SP, PC;
+        // Register Pair below
+        union RegisterPair{
+        //set low and high 8 bit registers or 16 bit register
+        uint16_t reg16;
+        struct{
+            uint8_t low;
+            uint8_t high;
+        };
+};
+        RegisterPair BC, DE, HL;
+        Registers();
+        //getters and setters for flags
         void setZeroFlag(bool flag);
         void setSubtractFlag(bool flag);
         void setHalfCarryFlag(bool flag);
@@ -13,19 +28,7 @@ class Registers{
         bool getHalfCarryFlag() const;
         bool getCarryFlag() const;
     private :
-        uint8_t A, B, C, D, E, H, L, F;
-        // Stack Pointer and Program Counter
-        uint16_t SP, PC;
-        // Register Pair below
-        uint16_t BC, DE, HL;
 };
-union RegisterPair{
-    //set low and high 8 bit registers or 16 bit register
-    uint16_t reg16;
-    struct{
-        uint8_t low;
-        uint8_t high;
-    };
-};
+
 #endif
 // Path: Registers.cpp
--- main.cpp
+++ main.cpp
@@ -8,9 +8,10 @@ int main(){
     reg.setSubtractFlag(true);
     reg.setHalfCarryFlag(true);
     reg.setCarryFlag(true);
+    reg.BC.high = 0x12;
     cout << "Zero Flag: " << reg.getZeroFlag() << endl;
     cout << "Subtract Flag: " << reg.getSubtractFlag() << endl;
     cout << "Half Carry Flag: " << reg.getHalfCarryFlag() << endl;
     cout << "Carry Flag: " << reg.getCarryFlag() << endl;
-    return 0;
+    return 0; 
 }
