fa0e36a05ba6bacec42efc3020b254fc0fd3a86e
DemitrisM <mavridis.d1@live.unic.ac.cy>
2024-02-23 20:19:48 +0200

    a lot of stuff

---
+++ build/CMakeFiles/GameBoyEmulator.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GameBoyEmulator.dir/Instructions.cpp.o"
+  "CMakeFiles/GameBoyEmulator.dir/Instructions.cpp.o.d"
+  "CMakeFiles/GameBoyEmulator.dir/Registers.cpp.o"
+  "CMakeFiles/GameBoyEmulator.dir/Registers.cpp.o.d"
+  "CMakeFiles/GameBoyEmulator.dir/main.cpp.o"
+  "CMakeFiles/GameBoyEmulator.dir/main.cpp.o.d"
+  "GameBoyEmulator"
+  "GameBoyEmulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GameBoyEmulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
